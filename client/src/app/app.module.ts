bc86183cadc71938fcd953c1fa63b157ee06ca91
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-01-28 11:52:22 +0100

    Scaffold CreditCardCompnent and AddressCompnent

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CreditCardComponent } from './credit-card/credit-card.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
